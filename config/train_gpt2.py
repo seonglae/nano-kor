ebb89e90dcde523e9db2d804f70e7620897b7fa9
seonglae <sungle3737@gmail.com>
2024-02-29 16:30:08 +0000

    feat: hankuk gpt with 2 3090 training

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -3,8 +3,8 @@
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
 wandb_log = True
-wandb_project = 'owt'
-wandb_run_name='gpt2-124M'
+wandb_project = 'nanogpt'
+wandb_run_name='hankuk-gpt'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
@@ -13,8 +13,8 @@ block_size = 1024
 gradient_accumulation_steps = 5 * 8
 
 # this makes total number of tokens be 300B
-max_iters = 600000
-lr_decay_iters = 600000
+max_iters = 60000
+lr_decay_iters = 60000
 
 # eval stuff
 eval_interval = 1000
