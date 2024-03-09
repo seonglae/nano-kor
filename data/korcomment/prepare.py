1a6620e824862d711c5cf346693c296b8e2bdec5
seonglae <sungle3737@gmail.com>
2024-03-09 13:36:18 +0900

    fix: proper num process for dataset maping

--- data/korcomment/prepare.py
+++ data/korcomment/prepare.py
@@ -40,7 +40,7 @@ def get_comment_dataset():
 
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
-num_proc = 64
+num_proc = os.cpu_count() // 2
 
 # number of workers in load_dataset() call
 # best number might be different from num_proc above as it also depends on NW speed.
